381327475c56997b09beaa6aae91b0746999e6d0
Ellen Nam <ellen.nam@westpac.com.au>
2025-04-22 22:54:40 +1000

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); //allow us to accept json data in the req.body
+
+app.use("/api/products", productRoutes);
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
